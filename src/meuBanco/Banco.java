e4e4f15ca759f35ba6cc2b2dd3bf2ec71af35d39
Daiana Eger Michels <daianaem.12@gmail.com>
2021-12-06 21:42:49 -0300

    Adiciona metodos a conta

--- src/meuBanco/Banco.java
+++ src/meuBanco/Banco.java
@@ -4,16 +4,25 @@ public class Banco {
 
 	public static void main(String[] args) {
 		Conta minhaConta = new Conta();
+		Conta segundaConta = new Conta();
 		
 		minhaConta.nomeDoTitular = "Daiana Eger Michels";
 		minhaConta.saldo = 100;
+		
+		segundaConta.nomeDoTitular = "Kate da Silva";
+		segundaConta.saldo = 50;
 		
 		minhaConta.sacar(50);
 		
 		minhaConta.depositar(75);
 		
+		minhaConta.transferir(100, segundaConta);
+		
 		System.out.printf("O saldo atual do cliente %s é de %.2f",
 				minhaConta.nomeDoTitular, minhaConta.getSaldo());
+		
+		System.out.printf("O saldo atual do cliente %s é de %.2f",
+				segundaConta.nomeDoTitular, segundaConta.getSaldo());
 	}
 
 }
--- src/meuBanco/Conta.java
+++ src/meuBanco/Conta.java
@@ -5,8 +5,21 @@ public class Conta {
 	String nomeDoTitular;
 	double saldo;
 	
-	void sacar(double valorSaque) {
+	boolean temSaldo(double valorSaque) {
+		if(valorSaque <= this.saldo) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	String sacar(double valorSaque) {
+		if (valorSaque <= this.saldo) {
 		this.saldo -= valorSaque;
+		return "Saldo atual: " + this.saldo;
+		} else {
+			return "Saldo insuficiente";
+		}
 	}
 	
 	void depositar(double valorDeposito) {
@@ -16,4 +29,10 @@ public class Conta {
 	double getSaldo() {
 		return this.saldo;
 	}//sem void o método deve retornar algo, ter um return
+
+	void transferir(double valorTransferencia, Conta contaDestino) {
+		if (temSaldo(valorTransferencia)) {
+			
+		}
+	}
 }
