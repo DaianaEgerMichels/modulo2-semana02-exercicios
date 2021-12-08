78f8c2dcef18c89ef715e60c168a07ed579906c1
Daiana Eger Michels <daianaem.12@gmail.com>
2021-12-08 15:36:13 -0300

    Adiciona comentarios

--- src/aula07/TestDrive.java
+++ src/aula07/TestDrive.java
@@ -2,8 +2,18 @@ package aula07;
 
 public class TestDrive {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {//classe para testar a classe Carro
+		Carro car = new Carro(110); //passar a velocidade máxima, parametro do construtor
+		//metodos da classe Carro:
+		car.ligar();
+		car.acelerar(80);
+		car.frear(10);
+		car.buzinar();
+		car.desligar();
+		
+		
+		System.out.println(car.velocidadeAtual);
+		
 
 	}
 
