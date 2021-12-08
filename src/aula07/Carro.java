2624994dd8450af7e97829fc94e005ed6b99e242
Daiana Eger Michels <daianaem.12@gmail.com>
2021-12-08 15:38:54 -0300

    Altera classe carro

--- src/aula07/Carro.java
+++ src/aula07/Carro.java
@@ -1,5 +1,51 @@
 package aula07;
 
 public class Carro {
-
+	String cor;
+	String modelo;
+	boolean ligado;
+	int velocidadeAtual;
+	int velocidadeMaxima;
+	
+	Carro(int velocidadeMax){
+		this.velocidadeMaxima += velocidadeMax;
+	}
+	
+	void ligar(){
+		if (!this.ligado) {
+			this.ligado = true;
+		} 
+	}
+	
+	void desligar() {
+		if (this.ligado) {
+			this.ligado = false;
+			this.velocidadeAtual = 0;
+		}
+	}
+	
+	void acelerar(int velocidadeAdd) {
+		if(this.ligado) {
+			if(this.velocidadeAtual + velocidadeAdd >= this.velocidadeMaxima) {
+				this.velocidadeAtual = this.velocidadeMaxima;
+				} else {
+				this.velocidadeAtual += velocidadeAdd;
+			}
+		}
+	}
+	
+	void frear(int velocidadeDecremental) {
+		if(this.ligado) {
+			if(velocidadeAtual - velocidadeDecremental <= 0) {
+				this.velocidadeAtual = 0;
+			} else {
+				this.velocidadeAtual -= velocidadeDecremental;
+			}
+		}
+		
+	}
+	
+	void buzinar() {
+		System.out.println("fon fon");
+	}
 }
